fc3baddf40acd0fbec081921eee315ece06fcfdb
ekta-verma <Ekta_Verma@external.mckinsey.com>
2013-10-24 14:16:57 -0700

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Karneval2013::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Karneval2013::Application.config.secret_key_base = '221e1ada0463cceaa3566d2d20d538bdc8a6aba3aa489f870a5b356ab4947c0a67d58b58824176c1d6a52a875476490ac82d0dee0c2b9ced55468d8b8cf689f3'
